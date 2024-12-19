115ad73b5893dee38ffb1ebfd9e4b713bdefa51c
divyanshmehta355 <devmehta1749@gmail.com>
2024-12-20 01:46:05 +0530

    ✅ import errors solved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const Home = () => {
-  return (
-    <div className='min-h-screen w-full'>Home</div>
-  )
-}
+  return <div className="min-h-screen w-full">Home</div>;
+};
 
-export default Home
+export default Home;
